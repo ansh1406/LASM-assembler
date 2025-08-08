07f2e7a8f6906252838c661551947b0ff649afef
Ansh Swaroop <anshswaroop14@gmail.com>
2025-08-09 02:34:33 +0530

    Corrected the assembly file extension

--- utils.py
+++ utils.py
@@ -3,16 +3,16 @@ from gui import asmEditor
 
 
 def open_file():
-    file_path = askopenfilename(filetypes=[("LM1 assembly", "*.lm1"), ("All Files", "*.*")])
+    file_path = askopenfilename(filetypes=[("LM1 assembly", "*.lasm"), ("All Files", "*.*")])
     if file_path:
         with open(file_path, 'r') as file:
             asmEditor.delete("1.0", "end")
             asmEditor.insert("1.0", file.read())
 
 def save_file():
-    file_path = asksaveasfilename(filetypes=[("LM1 assembly", "*.lm1"), ("All Files", "*.*")])
-    if not file_path.endswith(".lm1"):
-        file_path += ".lm1"
+    file_path = asksaveasfilename(filetypes=[("LM1 assembly", "*.lasm"), ("All Files", "*.*")])
+    if not file_path.endswith(".lasm"):
+        file_path += ".lasm"
     if file_path:
         with open(file_path, 'w') as file:
             file.write(asmEditor.get("1.0", "end"))
